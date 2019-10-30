4d819639dd9fd1da23fcf2ff2bc77bde7f497cf5
Alex Koskovich <zvnexus@outlook.com>
2019-10-30 17:55:19 -0400

    Bring back method getSubIdFromNetworkRequest.
    Don't remove the new integer, but bring back the old one so QTI
    telephony can reference it.
    
    Change-Id: I5ee25f3337dfd1958ba16340f25712ddd9486857

--- src/java/com/android/internal/telephony/PhoneSwitcher.java
+++ src/java/com/android/internal/telephony/PhoneSwitcher.java
@@ -995,7 +995,12 @@ public class PhoneSwitcher extends Handler {
         return phoneId;
     }
 
-   protected int getSubIdFromNetworkSpecifier(NetworkSpecifier specifier) {
+    protected int getSubIdFromNetworkRequest(NetworkRequest networkRequest) {
+        NetworkSpecifier specifier = networkRequest.networkCapabilities.getNetworkSpecifier();
+        return getSubIdFromNetworkSpecifier(specifier);
+    }
+
+    protected int getSubIdFromNetworkSpecifier(NetworkSpecifier specifier) {
         if (specifier == null) {
             return DEFAULT_SUBSCRIPTION_ID;
         }
