369a4157cb1beac8da06d0eb14b93cb61f7a613a
Bruno Martins <bgcngm@gmail.com>
2020-06-11 10:48:56 +0100

    Avoid SubscriptionManager#getUriForSubscriptionId calls with invalid subIds
    It seems like the SIM card can take longer to load on certain legacy CDMA devices,
    thus causing the subscription ID not to be valid during early telephony init.
    
    Just skip calling SubscriptionController#setMccMnc or setCountryIso at that point,
    so to avoid any subsequent calls to SubscriptionManager#getUriForSubscriptionId with
    an invalid subId, which then makes the phone process crash. Once the SIM is loaded,
    SubscriptionInfoUpdater will take care of updating the DB with the proper URI.
    
    Change-Id: I8bd71127aecff9577bb0d3b9d7cab7a01a076e87

--- src/java/com/android/internal/telephony/dataconnection/DataConnection.java
+++ src/java/com/android/internal/telephony/dataconnection/DataConnection.java
@@ -320,12 +320,12 @@ public class DataConnection extends StateMachine {
     static final int EVENT_NR_STATE_CHANGED = BASE + 27;
     static final int EVENT_DATA_CONNECTION_METEREDNESS_CHANGED = BASE + 28;
     static final int EVENT_NR_FREQUENCY_CHANGED = BASE + 29;
-    private static final int CMD_TO_STRING_COUNT = EVENT_NR_FREQUENCY_CHANGED - BASE + 1;
+ //   private static final int CMD_TO_STRING_COUNT = EVENT_NR_FREQUENCY_CHANGED - BASE + 1;
 
     protected static final int EVENT_RETRY_CONNECTION = BASE + 30;
 
     private static final int CMD_TO_STRING_COUNT =
-            EVENT_RETRY_CONNECTION - BASE + 2;
+            EVENT_RETRY_CONNECTION - BASE + 1;
 
     private static String[] sCmdToString = new String[CMD_TO_STRING_COUNT];
     static {
